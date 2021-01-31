0f50108844b94d504b3ad137883acfdf38bb57b6
Paula Barbosa Mattos <paulamat@gmail.com>
2021-01-31 15:58:10 -0300

    Arquivos projeto taxa de juros

---
+++ TaxaJuros/Aplicacao/AplicTaxaJuros.cs
@@ -0,0 +1,14 @@
+﻿namespace TaxaJuros.Aplicacao
+{
+    public class AplicTaxaJuros : IAplicTaxaJuros
+    {
+        public AplicTaxaJuros()
+        {
+        }
+
+        public decimal TaxaJuros()
+        {
+            return 0.01M;
+        }
+    }
+}
---
+++ TaxaJuros/Controllers/TaxaJurosController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaxaJuros.Aplicacao;
+
+namespace TaxaJuros.Controllers
+{
+    [Route("api/taxaJuros")]
+    public class TaxaJurosController: ControllerBase
+    {
+        public readonly IAplicTaxaJuros _aplicTaxaJuros;
+
+        public TaxaJurosController(IAplicTaxaJuros aplicTaxaJuros)
+        {
+            _aplicTaxaJuros = aplicTaxaJuros;
+        }
+
+        [HttpGet]
+        public ActionResult<decimal> TaxaJuros()
+        {
+            var ret = _aplicTaxaJuros.TaxaJuros();
+            return ret;  
+        }
+    }
+}
