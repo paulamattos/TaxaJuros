a5b5379bc33e2b01da1fad92b0736c39b3c7c625
Paula Barbosa Mattos <paulamat@gmail.com>
2021-02-01 23:00:33 -0300

    Teste de integração

---
+++ TaxaJuros.Test.Integracao/TestContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TaxaJuros.Test.Integracao
+{
+    public class TestContext
+    {
+        public HttpClient Client { get; set; }
+        private TestServer _server;
+
+        public TestContext()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = _server.CreateClient();
+        }
+    }
+}
---
+++ TaxaJuros.Test.Integracao/Tests/TaxaJurosTest.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TaxaJuros.Test.Integracao.Tests
+{
+    public class TaxaJurosTest
+    {
+        private readonly TestContext _testContext;
+        private readonly string _controller = "/taxaJuros";
+
+        public TaxaJurosTest()
+        {
+            _testContext = new TestContext();
+        }
+
+        [Fact]
+        public async Task TaxaJuros_DeveRetornar0_01()
+        {
+            var response = await _testContext.Client.GetAsync(_controller);
+
+            response.EnsureSuccessStatusCode();
+
+            var taxa = JsonConvert.DeserializeObject<decimal>(await response.Content.ReadAsStringAsync());
+
+            Assert.Equal(0.01m, taxa);
+        }
+    }
+}
